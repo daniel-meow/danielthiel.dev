67ec76ef703f9884db82880585ee840f2da20c06
daniel <dthiel.berlin@gmail.com>
2022-08-12 22:07:55 +0200

    Initial commit from Create Next App

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+const Home: NextPage = () => {
+  return (
+    <div className="p-8 h-[100vh] flex justify-start items-end border-2 bg-rose-900">
+      <p className="">under construction.</p>
+    </div>
+  );
+};
+
+export default Home;
