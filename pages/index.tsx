f63be61338cd1251b1024e963cb6df8bc4bb51cc
daniel <dthiel.berlin@gmail.com>
2022-08-13 12:39:47 +0200

    get static props

--- pages/index.tsx
+++ pages/index.tsx
@@ -22,4 +22,10 @@ const Home: NextPage = () => {
   );
 };
 
+export async function getStaticProps() {
+  return {
+    props: {}, // will be passed to the page component as props
+  };
+}
+
 export default Home;
