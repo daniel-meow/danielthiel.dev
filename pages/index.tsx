261ef3916231ca9c87ed39e92c7e29cfc0aa3a42
daniel <dthiel.berlin@gmail.com>
2022-08-13 12:31:19 +0200

    under construction

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,24 @@
+import { useEffect } from "react";
 import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
+import { Text } from "../components/atoms/Text";
 const Home: NextPage = () => {
+  useEffect(() => {
+    document.documentElement.style.setProperty(
+      "--vh",
+      window.innerHeight * 0.01 + "px"
+    );
+  });
   return (
-    <div className="p-8 h-screen flex justify-start items-end bg-rose-900">
-      <p className="antialiased">under construction.</p>
-    </div>
+    <>
+      <div className="p-8 h-screen w-screen bg-rose-900 flex flex-col justify-between">
+        <div className="flex justify-end items-start">
+          <Text color="white">Daniel Thiel. Front End Developer.</Text>
+        </div>
+        <div className="flex justify-start items-end">
+          <Text color="white">Under construction.</Text>
+        </div>
+      </div>
+    </>
   );
 };
 
